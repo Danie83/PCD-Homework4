267a5a85dc5cf00131f5b2981e1a5d86314fc40c
Filimon Dănuț-Dumitru <60850899+Danie83@users.noreply.github.com>
2023-05-01 17:58:19 +0300

    added form controller and page (testing)

---
+++ scripts/controllers/FormController.js
@@ -0,0 +1,32 @@
+const { WebcController } = WebCardinal.controllers;
+
+const model = {
+    todo: {
+        description: "Task:",
+        placeholder: "Add todo...",
+    },
+    days: {
+        description: "Days for completion:",
+        placeholder: "Add deadline day..."
+    },
+};
+
+var items = []
+
+export default class FormController extends WebcController {
+    constructor(element, history) {
+        super(element, history);
+        this.setModel(JSON.parse(JSON.stringify(model)));
+
+        this.onTagClick("submit", () => {
+            const todoData = this.model.toObject("todo");
+            const daysData = this.model.toObject("days");
+            let info = {
+                todo: todoData.value,
+                days: daysData.value
+            };
+            items.push(info);
+            console.log(items);
+        });
+    }
+}
