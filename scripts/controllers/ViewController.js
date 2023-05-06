a36c92c90929c28189ccda910704942ec313ed53
Filimon Dănuț-Dumitru <60850899+Danie83@users.noreply.github.com>
2023-05-06 11:40:32 +0300

    small change

--- scripts/controllers/ViewController.js
+++ scripts/controllers/ViewController.js
@@ -1,6 +1,5 @@
 // const { WebcController } = WebCardinal.controllers;
 
-
 // class view extends WebcController {
 //   initializeModel = () => ({ cards: [] });
 
@@ -26,58 +25,50 @@
 
 // export default view;
 
-
-
-
-
 const { WebcController } = WebCardinal.controllers;
 
-
-class view extends WebcController {
+export default class ViewController extends WebcController {
   getModel = (_) => ({
     input: {
-        type: "text",
-        value: "0",
-        style: "border: 1px solid #333",
+      type: "text",
+      value: "0",
+      style: "border: 1px solid #333",
     },
     task_list: [
-        {
-          task: "Title 1",
-          duration: "title--bigger",
-        },
-        {
-          task: "Title 1",
-          duration: "title--bigger",
-        },
-        {
-          task: "Title 1",
-          duration: "title--bigger",
-        },
+      {
+        task: "Title 1",
+        duration: "title--bigger",
+      },
+      {
+        task: "Title 1",
+        duration: "title--bigger",
+      },
+      {
+        task: "Title 1",
+        duration: "title--bigger",
+      },
     ],
-  })
+  });
   constructor(element, history) {
     super(element, history);
 
     this.model = this.getModel();
 
     Array.from(Array(this.model.task_list.length)).forEach((_, idx) => {
-        this.model.addExpression(
-            `task_list.${idx}.formattedInputValue`,
-            () => {
-                return `Current value is: ${this.model.input.value * idx + 1}`;
-            },
-            "input.value"
-        );
-        this.model.addExpression(
-            `task_list.${idx}.formattedClass`,
-            () => {
-                return `class-${this.model.input.value * idx + 1}`;
-            },
-            "input.value"
-        );
+      this.model.addExpression(
+        `task_list.${idx}.formattedInputValue`,
+        () => {
+          return `Current value is: ${this.model.input.value * idx + 1}`;
+        },
+        "input.value"
+      );
+      this.model.addExpression(
+        `task_list.${idx}.formattedClass`,
+        () => {
+          return `class-${this.model.input.value * idx + 1}`;
+        },
+        "input.value"
+      );
     });
   }
-
 }
-
-export default view;
