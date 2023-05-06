26ad7e884e596e9234e1ecba1ae0127f2fb3e42a
Filimon Dănuț-Dumitru <60850899+Danie83@users.noreply.github.com>
2023-05-06 12:16:21 +0300

    added css, included data-if tag for form-control

--- scripts/controllers/FormController.js
+++ scripts/controllers/FormController.js
@@ -9,6 +9,7 @@ const model = {
         description: "Days for completion:",
         placeholder: "Add deadline day..."
     },
+    formMessage: false,
 };
 
 var items = []
@@ -21,10 +22,31 @@ export default class FormController extends WebcController {
         this.onTagClick("submit", () => {
             const todoData = this.model.toObject("todo");
             const daysData = this.model.toObject("days");
+
+            if (todoData.value == undefined || 
+                daysData.value == undefined ||
+                todoData.value.length == 0 ||
+                daysData.value.length == 0)
+            {
+                if (!this.model.formMessage)
+                {
+                    this.model.formMessage = true;
+                }
+                return;
+            }
+            else
+            {
+                if (this.model.formMessage)
+                {
+                    this.model.formMessage = false;
+                }
+            }
+
             let info = {
                 todo: todoData.value,
                 days: daysData.value
             };
+
             items.push(info);
             console.log(items);
         });
